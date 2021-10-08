95b2be976909130498c9e65f24ce509b81a1998f
Rynco Maekawa <lynzrand@outlook.com>
2021-10-08 23:11:59 +0800

    feat: scaffold VM & IR

--- crates/inkstone-ir/src/lib.rs
+++ crates/inkstone-ir/src/lib.rs
@@ -1,7 +1,83 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub struct Function {}
+
+/// A virtual register slot in function
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+#[repr(transparent)]
+pub struct Reg(u32);
+
+/// An SSA instruction.
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[repr(C)]
+pub struct Inst {
+    /// Instruction Kind
+    kind: InstKind,
+    /// Instruction Flags
+    flags: InstFlags,
+    /// Destination
+    dest: Reg,
+    /// The 1st argument
+    p1: Reg,
+    /// The 2nd argument
+    p2: Reg,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Copy)]
+#[repr(u16)]
+pub enum InstKind {
+    Nop = 0,
+    // int
+    IAdd = 16,
+    ISub,
+    IMul,
+    IDiv,
+    IAnd,
+    IOr,
+    IXor,
+    IEq,
+    INeq,
+    IGt,
+    ILt,
+    IGe,
+    ILe,
+    // float
+    FAdd = 32,
+    FSub,
+    FMul,
+    FDiv,
+    FEq,
+    FNeq,
+    FGt,
+    FLt,
+    FGe,
+    FLe,
+    // bool
+    BAnd,
+    BOr,
+    BXor,
+    BNot,
+    // scope
+    ScopeNew = 48,
+    ScopeLoad,
+    ScopeStore,
+    // tuple
+    /// `dest <- TupleNew(p1=length, _)`
+    TupleNew,
+    // object
+    /// `dest[p1] <- p2`
+    SubscriptSet,
+    /// `dest <- p1[p2]`
+    SubscriptLoad,
+    // closure
+    /// `dest <- ClosureNew(p1=function, p2=scope)`
+    ClosureNew,
+    // flow control
+    Tail = 96,
+    TailIf,
+    Call,
+}
+
+bitflags::bitflags! {
+    pub struct InstFlags: u16 {
+
     }
 }
--- crates/inkstone-vm/src/lib.rs
+++ crates/inkstone-vm/src/lib.rs
@@ -1,7 +1,50 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::collections::{HashMap, VecDeque};
+use std::sync::Arc;
+
+use smol_str::SmolStr;
+
+pub enum Val {
+    Nil,
+    Bool(bool),
+    Int(i64),
+    Float(f64),
+    String(Gc<String>),
+    Tuple(TupleRef),
+    Array(Gc<Array>),
+    Object(Gc<Object>),
+    Scope(Gc<Scope>),
+    Closure(Gc<Closure>),
+    Symbol(u64),
 }
+
+pub struct TupleRef {
+    arity: u16,
+    val: *mut Val,
+}
+
+pub struct Array {
+    val: VecDeque<Val>,
+}
+
+pub struct Object {
+    proto: Gc<Object>,
+    val: HashMap<u64, Val>,
+}
+
+pub struct Closure {
+    func: Gc<Function>,
+    scope: Gc<Scope>,
+}
+
+pub struct Scope {
+    parent: Gc<Scope>,
+    val: Vec<Val>,
+}
+
+pub struct Gc<T>(*mut GcValue<T>);
+
+pub struct GcValue<T> {
+    val: T,
+}
+
+pub struct Function {}
